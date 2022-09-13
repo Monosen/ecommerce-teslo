b5f186ff0ca3525664e4e094dfabbcf1eb7435b1
Monosen <edinson.bolanos00@usc.edu.co>
2022-09-12 22:42:45 -0500

    321

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -13,7 +13,17 @@ import { CartList, OrderSummary } from '../../components/cart'
 import { ShopLayout } from '../../components/layouts'
 import { CreditCardOffOutlined } from '@mui/icons-material'
 
-const OrderPage = () => {
+import { GetServerSideProps, NextPage } from 'next'
+import { getSession } from 'next-auth/react'
+import { dbOrders } from '../../database'
+import { IOrder } from '../../interface'
+
+interface Props {
+	order: IOrder
+}
+
+const OrderPage: NextPage<Props> = ({ order }) => {
+	console.log(order)
 	return (
 		<ShopLayout
 			title='Resumen de la orden 3242123123'
@@ -91,6 +101,50 @@ const OrderPage = () => {
 			</Grid>
 		</ShopLayout>
 	)
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+	req,
+	query
+}) => {
+	const { id = '' } = query
+
+	const session: any = await getSession({ req })
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: `/auth/login?p=/orders/${id}`,
+				permanent: false
+			}
+		}
+	}
+
+	const order = await dbOrders.getOrderById(id.toString())
+
+	if (!order) {
+		return {
+			redirect: {
+				destination: `/orders/history`,
+				permanent: false
+			}
+		}
+	}
+
+	if (order.user !== session.user._id) {
+		return {
+			redirect: {
+				destination: `/orders/history`,
+				permanent: false
+			}
+		}
+	}
+
+	return {
+		props: {
+			order
+		}
+	}
 }
 
 export default OrderPage
